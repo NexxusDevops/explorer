3dacfd7685c789793a3564ae552d9f7b09e42088
NexxusDevops <125220205+NexxusDevops@users.noreply.github.com>
2023-02-13 12:58:05 -0700

    Update Staking.vue

--- src/views/Staking.vue
+++ src/views/Staking.vue
@@ -2,7 +2,7 @@
   <div>
     <b-card
       v-if="pingVals && pingVals.length > 0"
-      title="❤️ Helping Ping.pub By Staking ❤️"
+      title="❤️ Helping BlockchainNodes.io By Staking ❤️"
       class="overflow-auto"
     >
       <b-table
